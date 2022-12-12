f143794a4e0332d36fd3fac45a04e32809aeb29f
asdasd-mjeesh <201300@nuos.edu.ua>
2022-12-12 08:21:12 +0200

    added beanFactory

--- src/main/java/com/access_security/service/AccountCommunicationService.java
+++ src/main/java/com/access_security/service/AccountCommunicationService.java
@@ -3,8 +3,6 @@ package com.access_security.service;
 import com.access_security.model.request.account.AccountRequest;
 import com.access_security.model.response.account.AccountResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -12,38 +10,29 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class AccountCommunicationService implements AccountService {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-    private OkHttpClient okHttpClient;
-    private final Long responseTimeAwait = 10L;
+    public final OkHttpClient okHttpClient;
+    public final ObjectMapper objectMapper;
 
     @Value("${http.communication.account.api.url}")
     private String accountApiUrl;
 
-    @PostConstruct
-    private void setup() {
-        okHttpClient = new OkHttpClient().newBuilder()
-                .connectTimeout(responseTimeAwait, TimeUnit.SECONDS)
-                .readTimeout(responseTimeAwait, TimeUnit.SECONDS)
-                .writeTimeout(responseTimeAwait, TimeUnit.SECONDS)
-                .build();
+    public AccountCommunicationService(OkHttpClient okHttpClient, ObjectMapper objectMapper) {
+        this.okHttpClient = okHttpClient;
+        this.objectMapper = objectMapper;
     }
-    
+
     @Override
     public Optional<AccountResponse> create(AccountRequest account) {
         try {
-            var accountJsonString = OBJECT_MAPPER.writeValueAsString(account);
+            var accountJsonString = objectMapper.writeValueAsString(account);
             var body = RequestBody.create(accountJsonString, JSON);
             var request = new Request.Builder()
                     .url(accountApiUrl + "/")
@@ -54,7 +43,7 @@ public class AccountCommunicationService implements AccountService {
 
             if (response.code() == HttpStatus.CREATED.value()) {
                 AccountResponse accountResponse =
-                        OBJECT_MAPPER.readValue(Objects.requireNonNull(response.body()).string(), AccountResponse.class);
+                        objectMapper.readValue(Objects.requireNonNull(response.body()).string(), AccountResponse.class);
                 return Optional.ofNullable(accountResponse);
             }
             return Optional.empty();
